0a0884f38726652c91a4144eb706cc4533481a13
Vanshika Chaudhary <vanshika.chaudharywork@gmail.com>
2025-08-25 16:26:57 +0530

    Added LL problems

--- Linkedlist/Linkedlist.cpp
+++ Linkedlist/Linkedlist.cpp
@@ -51,15 +51,40 @@ Node* Add1toLL(Node* head){
     }
     head=ReverseLL(head);
     return head;
+}
+Node* AddTwoLL(Node* list1, Node* list2){
+    Node* dummy=new Node(0);
+    Node* temp=dummy;
+    int carry=0;
 
+    while(list1!=NULL || list2!=NULL || carry){
+        int sum=0;
+        if(list1!=NULL){
+            sum+=list1->data;
+            list1=list1->next;
+        }
+        if(list2!=NULL ){
+            sum+=list2->data;
+            list2=list2->next;
+        }
+        sum+=carry;
+        carry=sum/10;
+        Node* newnode= new Node(sum%10);
+        temp->next=newnode;
+        temp=temp->next;
+    }
+    return dummy->next;
 }
 
 int main() {
-    Node*head= new Node(1);
-    head->next=new Node(9);
-    head->next->next=new Node(9);
+    Node*head1= new Node(1);
+    head1->next=new Node(9);
+    head1->next->next=new Node(9);
 
-    Node * ans=Add1toLL(head);
+    Node* head2= new Node(2);
+    head2->next= new Node(3);
+
+    Node * ans=AddTwoLL(head1, head2);
     Node* temp=ans;
     while(temp!=nullptr){
         cout<<temp->data;
