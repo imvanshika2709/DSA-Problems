314a0fb56030c540907bb766925723e51ed5cddc
Vanshika Chaudhary <vanshika.chaudharywork@gmail.com>
2025-08-30 12:56:54 +0530

    Added LL problems

--- Linkedlist/Linkedlist.cpp
+++ Linkedlist/Linkedlist.cpp
@@ -137,6 +137,17 @@ int FindLength(Node* head){
     return 0;
 }
 
+Node* Yintersection(Node* head1, Node* head2){
+    Node* d1=head1;
+    Node* d2=head2;
+
+    while(d1!=d2){
+        d1=d1==NULL ? head2: d1->next;
+        d2=d2==NULL ? head1: d2->next;
+    }
+    return d1;
+}
+
 int main() {
     Node* head1=new Node(1);
     Node* first= new Node(2);
