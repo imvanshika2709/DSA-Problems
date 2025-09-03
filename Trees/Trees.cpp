73989c0a01d3c0305ef2e2addfae0ba7b7466aee
Vanshika Chaudhary <vanshika.chaudharywork@gmail.com>
2025-09-03 21:01:58 +0530

    Added Trees problems

--- Trees/Trees.cpp
+++ Trees/Trees.cpp
@@ -50,6 +50,18 @@ Node* InvertTree(Node* root){
 
  */
 }
+Node* lowestCommonAncestor(Node* root, Node* p, Node* q) {
+	if(root==NULL || root==p || root==q) return root;
+	
+	Node* left=lowestCommonAncestor(root->left, p, q);
+	Node* right=lowestCommonAncestor(root->right, p,q);
+	
+	if(left==NULL) return right;
+
+	else if(right==NULL) return left;
+    
+	else return root;
+}
 int main() {
 
 
