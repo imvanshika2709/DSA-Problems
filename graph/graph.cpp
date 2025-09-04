70788ca25919e0bee24cf0059737c6a1fb208ec6
Vanshika Chaudhary <vanshika.chaudharywork@gmail.com>
2025-09-05 00:12:52 +0530

    Added graph problems

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -26,6 +26,50 @@ vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
     return dist;
 }
 
+
+
+
+bool isValid(int x, int y, vector<vector<int>>& surface){
+	int n=surface.size();
+	int m=surface[0].size();
+	
+	return (x>=0 && x<n && y>=0 && y<m);
+}
+void dfs(int x, int y, vector<vector<int>>& surface, vector<vector<bool>>& visited) {
+	visited[x][y] = true;
+	int dx[4] = {-1, 1, 0, 0};
+	int dy[4] = {0, 0, -1, 1};
+	
+	for (int k = 0; k < 4; k++) {
+        int nx = x + dx[k];
+        int ny = y + dy[k];
+		
+		if (isValid(nx, ny, surface) && !visited[nx][ny] && surface[nx][ny] == 1){
+            dfs(nx, ny, surface, visited);
+		}
+	}
+}
+
+int getNumberOfIslands(vector<vector<int>> &surface) {
+	
+	int n=surface.size();
+	int m=surface[0].size();
+	vector<vector<bool>> visited(n, vector<bool>(m, false));
+
+	
+	int count=0;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			if(!visited[i][j] && surface[i][j]==1){
+				dfs(i, j, surface, visited);
+				count++;
+			}
+		}
+	}
+	return count;
+	
+}
+
 int main() {
     
     return 0;
