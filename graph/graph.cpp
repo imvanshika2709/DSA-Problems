89957339eb2e5320a0293f01e1b5802d87a4d5da
Vanshika Chaudhary <vanshika.chaudharywork@gmail.com>
2025-09-09 21:46:15 +0530

    Added graph problems

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -167,6 +167,50 @@ bool DetectCycleinDirected(int V,vector<int> adj[]){
 	return false;
 }
 
+bool CourseSchedule_util(vector<vector<int>>& adj) {
+    int V = adj.size();
+    queue<int> q;
+    vector<int> indegree(V, 0);
+    vector<int> topo;
+
+    for (int i = 0; i < V; i++) {
+        for (auto it : adj[i]) {
+            indegree[it]++;
+        }
+    }
+
+    for (int i = 0; i < V; i++) {
+        if (indegree[i] == 0) q.push(i);
+    }
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        topo.push_back(node);
+        for (auto it : adj[node]) {
+            indegree[it]--;
+            if (indegree[it] == 0) q.push(it);
+        }
+    }
+
+    return topo.size() != V;  // true if cycle
+}
+
+bool CourseSchedule(int n, vector<vector<int>> &prerequisites) {
+    // add your logic here
+	vector<vector<int>> adj(n);
+    
+    // build graph: b → a
+    for (auto& pre : prerequisites) {
+        int a = pre[0], b = pre[1];
+        adj[b].push_back(a);
+    }
+
+    // if graph has cycle → cannot finish all courses
+    return !CourseSchedule_util(adj);
+}
+
 int main() 
 {
 	int V=6;
