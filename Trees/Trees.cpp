ff0b8ef4b7b9e1a96bb4871526228b4383020466
Vanshika Chaudhary <vanshika.chaudharywork@gmail.com>
2025-09-11 14:53:32 +0530

    Added trees problems

--- Trees/Trees.cpp
+++ Trees/Trees.cpp
@@ -128,17 +128,41 @@ Node* lowestCommonAncestor(Node* root, Node* p, Node* q) {
     
 	else return root;
 }
+void KthSmallest_util(Node* root, int& counter, int& k, int ksmallest){
+    if(root== NULL || counter>=k) return ;
+
+    KthSmallest_util(root->left, counter, k, ksmallest);
+
+    counter++;
+    if(counter==k){
+        ksmallest=root->val;
+        return;
+    }
+
+    KthSmallest_util(root->right, counter, k, ksmallest);
+
+}
+int KthSmallest(Node* root, int k){
+    int counter=0;
+    int ksmallest=0;
+
+    KthSmallest_util(root, counter, k, ksmallest);
+    return ksmallest;
+
+}
 int main() {
 
 
-   Node* root = new Node(-10);
-    root->left = new Node(9);
-    root->right = new Node(20);
-    root->right->left=new Node(15);
-    root->right->right->right=new Node(7);
-
-   int ans=maxPathSum(root);
-   cout<<ans;
+    Node* root = new Node(10);
+    root->left = new Node(5);
+    root->right = new Node(13);
+    root->left->left = new Node(3);
+    root->left->left->left = new Node(2);
+    root->left->left->right = new Node(4);
+    root->left->right = new Node(6);
+    root->left->right->right = new Node(9);
+    root->right->left = new Node(11);
+    root->right->right = new Node(14);
 
 
 
