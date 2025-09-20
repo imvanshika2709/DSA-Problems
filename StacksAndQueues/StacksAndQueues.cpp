d620d7b133ef5559137231b13c242f59006a8122
Vanshika Chaudhary <vanshika.chaudharywork@gmail.com>
2025-09-20 22:25:53 +0530

    Added Stacks problems

--- StacksAndQueues/StacksAndQueues.cpp
+++ StacksAndQueues/StacksAndQueues.cpp
@@ -1,4 +1,21 @@
 #include <bits/stdc++.h>
+// Stacks using arrays
+// LIFO
+
+
+class stack{
+
+    int size;
+    int *arr;
+    int top=-1;
+
+    stack(){
+        size=1000;
+        arr=new int [size];
+        
+    }
+
+};
 
 int main() {
     
